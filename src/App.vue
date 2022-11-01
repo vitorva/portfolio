a20044de65384043d132ba1cfbbe157bf82da579
vitorva <vitor.vazafonso@gmail.com>
2022-11-01 16:34:11 +0100

    responsive site

--- src/App.vue
+++ src/App.vue
@@ -5,81 +5,112 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <div><img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="60" height="60" /></div>
+    <div>
+      <img alt="Vue logo" class="logo" src="@/assets/mail.svg" width="30" height="30" />
+      <img alt="Vue logo" class="logo" src="@/assets/github.svg" width="30" height="30" />
+    </div>
+    <!--
     <div class="wrapper">
-      <HelloWorld msg="You did it !!!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
+-->
   </header>
 
-  <RouterView />
+  <div class="content">
+
+    <div class="box box-top">
+      <h1>Welcome .</h1>
+
+      <p>My name is Vitor Vaz Afonso. I’m a software enginner from Switzerland.
+
+        I recently graduated from HEIG-VD (Bachelor of Applied Science). I have always been
+        passionate about technology and development.
+      </p>
+    </div>
+
+    <div class="box box-bottom">
+      <h1>Projects</h1>
+    </div>
+  </div>
+
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  margin-right: 20px;
 }
 
-nav {
+.content {
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.box {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  border-color: #d9d9d9b2;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+
+/*
+.box-top::before {
+  border-top: 3px solid black;
+}
+*/
+
+
+.box-top::before {
+  border-top: solid #d9d9d9b2;
+  content: '';
+  display: block;
+  margin-right: 35%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+.box-bottom::after {
+  border-bottom: solid #d9d9d9b2;
+  content: '';
+  display: block;
+  margin-left: 35%;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+
+  .content {
+    flex-wrap: nowrap;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .box {
+    width: 45%;
+    height: 500px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .box-top {
+    border-top: solid;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+  .box-bottom {
+    border-bottom: solid;
+  }
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+
+  .box-top::before {
+    content: none;
+  }
+
+  .box-bottom::after {
+    content: none;
   }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,33 +8,17 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Welcome.
+
+
+      My name is Vitor Vaz Afonso. I’m a software enginner from Switzerland.
+
+      I recently graduated from HEIG-VD (Bachelor of Applied Science). I have always been
+      passionate about technology and development.
     </h3>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
+@media (min-width: 1024px) {}
 </style>
