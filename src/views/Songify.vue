9aaac62459df0727751fd54dfa67923d691b0f54
vitorva <vitor.vazafonso@gmail.com>
2022-11-07 17:54:14 +0100

    update

--- src/views/Songify.vue
+++ src/views/Songify.vue
@@ -7,11 +7,12 @@
         <p>
             A spotify-like using the SoundCloud API. </p>
 
-        <p class="technos"><a>Vue3 · &nbspVuex · &nbspExpress · &nbspSoundCloud API</a></p>
+        <p class="technos"><a>Vue3 · &nbspVuex · &nbspExpress · &nbspSoundCloud API · &nbspNetlify ·
+                &nbspDigitalOcean</a></p>
         <div>
             <a class="link" href="https://github.com/vitorva/songify"><img alt="Vue logo" class="logo"
                     src="@/assets/github.svg" width="25" height="25" /></a>
-            <a class="link" href="https://songifyy.herokuapp.com"><img alt="Vue logo" class="logo"
+            <a class="link" href="https://cerulean-brioche-be2c0e.netlify.app"><img alt="Vue logo" class="logo"
                     src="@/assets/links.svg" width="25" height="25" /></a>
         </div>
     </div>
