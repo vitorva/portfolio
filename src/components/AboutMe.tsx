3dfe443ea36556e60981ae4bac2c8f868218f000
vitorva <vitor.vazafonso@gmail.com>
2023-03-28 15:24:09 +0200

    upadtes css

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -13,14 +13,27 @@ function AboutMe({ aboutMeRef }: AboutMeProps) {
       className="aboutMe"
     >
       <h1>About me</h1>
-      <p className="about">
-        In my free time, I enjoy pursuing a variety of hobbies that allow me to
-        relax and discover new things. I like to hike and enjoy spending time in
-        nature. One of my new hobbies is to take photos of landscapes and scenes
-        that I encounter along the way.
-      </p>
-
-      <CarouselComponent />
+      <div className="about">
+        <p>
+          {" "}
+          My name is Vitor Vaz Afonso. I'm a Software Engineer from Switzerland.
+        </p>
+        <p>
+          {" "}
+          I gratuated from the{" "}
+          <a href="https://heig-vd.ch/">
+            University of Applied Sciences (HEIG-VD) in Yverdon-les-Bains
+          </a>{" "}
+          in Semptember 2022, with a Bachelor of Science HES-SO in Computer
+          Science, major in Software Engineering
+        </p>
+        <p>
+          {" "}
+          In my free time, I enjoy pursuing a variety of hobbies that allow me
+          to relax and discover new things. I particularly like to hike and
+          enjoy spending time in nature.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,6 +6,48 @@ function Skills({ skillsRef }: SkillsProps) {
   return (
     <div ref={skillsRef} style={{ scrollMarginTop: "10vh" }} className="skills">
       <h1>Skills</h1>
+      <div className="mySkills">
+        <div className="skill">
+          <h3>Technical knowledges</h3>
+
+          <p>A selection of technolgie I mainly use.</p>
+
+          <p>
+            <b>Languages :</b> JavaScript • TypeScript • Java • C++ • C# •
+            Python • Scala • HTML5 • CSS3 • PHP • Kotlin
+          </p>
+          <p>
+            <b>Frontend : </b> React • Redux • Vue.js • Vuex
+          </p>
+          <p>
+            <b>Backend : </b> Node.js • Express.js • Spring Boot • Maven •
+            GraphQL
+          </p>
+          <p>
+            <b>Databases : </b> MySQL • PostgreSQL • MongoDB • Neo4j
+          </p>
+          <p>
+            <b>Cloud : </b> AWS • GCP • Heroku • DigitalOcean
+          </p>
+          <p>
+            <b>Testing : </b> Jest • Enzyme • CodeceptJS • Puppeteer • Jmeter
+          </p>
+          <p>
+            <b>DevOps : </b> Git • Docker • CI/CD • Pipelines
+          </p>
+        </div>
+        <div className="skill">
+          <h3>General knowledges</h3>
+          <p>
+            {" "}
+            I acquired skills in many other technology-related fields.
+            Specifically, I have focused on machine learning, natural language
+            processing, mobile application development, cloud infrastructure and
+            Devops. Furthermore, I also learned AGILE methods and how to work in
+            collaboration through many group projects.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
